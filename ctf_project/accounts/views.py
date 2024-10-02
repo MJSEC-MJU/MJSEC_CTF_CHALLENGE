f3f915c4e8fb28fd916a01ce34fb4670ec6ed2a9
Jongcoding <ialleejy@gmail.com>
2024-10-02 09:08:13 +0900

    update mjsec logo

--- ctf_project/accounts/views.py
+++ ctf_project/accounts/views.py
@@ -1,27 +1,29 @@
-# accounts/views.py
-
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from django.contrib.auth.forms import AuthenticationForm
 
 def login_view(request):
     if request.user.is_authenticated:
         return redirect("challenges:feeds")
+    
     if request.method == 'POST':
         form = AuthenticationForm(data=request.POST)
         if form.is_valid():
             user = form.get_user()
             login(request, user)
+            messages.success(request, "로그인에 성공했습니다.")
             return redirect('challenges:feeds')
-        
+        else:
+            messages.error(request, "아이디 또는 패스워드가 올바르지 않습니다.")
+    
     else:
         form = AuthenticationForm()
+    
     return render(request, 'accounts/login.html', {'form': form})
 
 def logout_view(request):
     logout(request)
+    messages.success(request, "성공적으로 로그아웃되었습니다.")
     return redirect('accounts:login')
-
